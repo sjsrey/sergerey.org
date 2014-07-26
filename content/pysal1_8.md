85f079dc8e07b4bf84e693685241bb318f85a19d
Serge Rey <sjsrey@gmail.com>
2014-07-26 14:14:01 -0700

    start of new site

---
+++ content/pysal1_8.md
@@ -0,0 +1,14 @@
+Title: PySAL 1.8 Released
+Date: 2014-07-25
+Tags: pysal
+
+
+We normally ship [PySAL][pysal] on a six-month release cycle, once at the end of January and again the end of July. This year due to travel schedules (it is summer after all) we shipped 1.8 one week early. 
+
+A lot of work on the testing infrastructure, bug fixes and optimizations. One new [contrib  module][contrib] that provides for interaction between  PySAL and [clusterpy][cp].
+
+
+[cp]: http://www.rise-group.org/risem/clusterpy/
+[pysal]: http://pysal.readthedocs.org/en/v1.8/
+[contrib]: http://pysal.readthedocs.org/en/v1.8/library/contrib/index.html 
+
---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Serge Rey'
+SITENAME = u'Serge Rey'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Phoenix'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         )
+
+# Social widget
+SOCIAL = (('twitter', 'http://twitter.com/sergerey'),
+                    ('github', 'http://github.com/sjsrey'),)
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = "/Users/serge/pelican-themes/pelican-bootstrap3"
+
+DISPLAY_PAGES_ON_MENU=True
+DISPLAY_CATEGORIES_ON_MENU = False
+
